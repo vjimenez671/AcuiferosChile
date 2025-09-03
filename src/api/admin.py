f8b163c3ae9b4647452d807e1f647ac0309b4735
Vicente Jiménez <vicentejimenez.actor@gmail.com>
2025-09-03 13:01:59 +0000

    importacion de modelos en admin.py

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
 
 import os
 from flask_admin import Admin
-from .models import db, User, Post
+from .models import db, User, Post, Comment, Reaction
 from flask_admin.contrib.sqla import ModelView
 
 
@@ -13,6 +13,10 @@ def setup_admin(app):
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Post, db.session))
+    admin.add_view(ModelView(Comment, db.session))
+    admin.add_view(ModelView(Reaction, db.session))
+
+
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
