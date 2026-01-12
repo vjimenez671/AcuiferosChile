1b151e7a1fff250a2d2761729c502dcb5cd01f6a
Vicente Jimenez <vicentejimenez.actor@gmail.com>
2026-01-12 16:54:44 -0300

    modificaciones.3

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,22 +1,24 @@
 
 import os
+
 from flask_admin import Admin
-from .models import db, User, Post, Comment, Reaction
 from flask_admin.contrib.sqla import ModelView
+from flask_admin.theme import Bootstrap4Theme
+
+from .models import db, User, Post, Comment, Reaction
 
 
 def setup_admin(app):
-    app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
-    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+    # Secret key (Render la puede inyectar como env var)
+    app.secret_key = os.environ.get("FLASK_APP_KEY", "sample key")
 
-    # Add your models here, for example this is how we add a the User model to the admin
+    # Flask-Admin 2.x: se usa theme (Bootstrap4) y swatch para el color
+    theme = Bootstrap4Theme(swatch="cerulean")
+    admin = Admin(app, name="4Geeks Admin", theme=theme)
+
+    # Add your models here
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Post, db.session))
     admin.add_view(ModelView(Comment, db.session))
     admin.add_view(ModelView(Reaction, db.session))
 
-
-
-    # You can duplicate that line to add mew models
-    # admin.add_view(ModelView(YourModelName, db.session))
